d165e70c265060cd6e871581f5ec99e5d6028627
Tor Valamo <tor.valamo@gmail.com>
2017-10-29 18:20:27 +0100

    catch argument missing

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = new Proxy({}, {
 			try {
 				let methods  = require(`./lib/${module}`);
 				handleModule(original, promisified, methods);
-			} catch () {
+			} catch (e) {
 				return original;
 			}
 			
